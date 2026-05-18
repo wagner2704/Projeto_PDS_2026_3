37071187f369ebd556f61ad08228c2fc2a5de970
wagner2704 <gabriel.wagner@academico.ifpb.edu.br>
2026-05-18 11:18:15 -0300

    questao9.m

--- simulacoes/questao9.m
+++ simulacoes/questao9.m
@@ -1,17 +1,31 @@
+% =========================================================
+% QUESTAO 9
+% Analise de harmonicos em sinais discretos
+% =========================================================
+
 clc;
 clear;
 close all;
 
+% Numero de amostras
 N = 256;
+
+% Vetor discreto
 n = 0:N-1;
 
+% Frequencia fundamental
 f1 = 0.1;
+
+% Segunda harmonica
 f2 = 0.2;
 
+% Sinal composto
 x = sin(2*pi*f1*n) + 0.5*sin(2*pi*f2*n);
 
+% FFT do sinal
 X = fft(x);
 
+% Plot do espectro
 figure;
 plot(abs(X));
 
