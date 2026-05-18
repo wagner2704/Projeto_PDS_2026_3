aa6d1ba599acc4f3d6a6132f8cf26ae9de44eead
wagner2704 <gabriel.wagner@academico.ifpb.edu.br>
2026-05-18 11:18:52 -0300

    questao10.m

--- simulacoes/questao10.m
+++ simulacoes/questao10.m
@@ -1,20 +1,37 @@
+% =========================================================
+% QUESTAO 10
+% Analise espectral de sinal com ruido
+% =========================================================
+
 clc;
 clear;
 close all;
 
+% Frequencia de amostragem
 Fs = 1000;
 
-t = 0:1/Fs:1;
+% Vetor de tempo
+ t = 0:1/Fs:1;
 
-x = sin(2*pi*50*t) + 0.3*randn(size(t));
+% Geracao do sinal senoidal
+x = sin(2*pi*50*t);
 
+% Adicao de ruido gaussiano
+x = x + 0.3*randn(size(t));
+
+% Plot do sinal no dominio do tempo
 figure;
 plot(t,x);
 
-title('Sinal no Tempo');
+title('Sinal no Dominio do Tempo');
+xlabel('Tempo (s)');
+ylabel('Amplitude');
+grid on;
 
+% FFT do sinal
 X = fft(x);
 
+% Plot do espectro
 figure;
 plot(abs(X));
 
